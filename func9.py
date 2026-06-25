960e1381899c278c37377a6ef2d39b641d942443
saikriish14-spec <saikriish14@gmail.com>
2026-06-25 20:37:14 +0100

    ok

---
+++ func9.py
@@ -0,0 +1,10 @@
+a = input("Enter your name or enter your word:" )
+for i in a :
+    if a == "a":
+        print("a is found")
+        break
+    else:
+        print("a is not found")
+
+    
+          
